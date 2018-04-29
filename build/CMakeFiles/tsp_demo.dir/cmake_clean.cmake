13fc76f37ce192e1e84c71e07ad4baf9caabc26b
Tianyi Fang <fongtinyik@gmail.com>
2018-04-29 00:55:33 -0700

    Compile concorde TSP solver for mac

---
+++ build/CMakeFiles/tsp_demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tsp_demo.dir/src/tsp_demo.cpp.o"
+  "CMakeFiles/tsp_demo.dir/src/tsp.cpp.o"
+  "CMakeFiles/tsp_demo.dir/src/tour.cpp.o"
+  "../bin/tsp_demo.pdb"
+  "../bin/tsp_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tsp_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
